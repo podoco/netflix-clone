e7a4f92baa352a20e569c1e4cb4d3f1853b0a94e
podoco <103816228+podoco@users.noreply.github.com>
2022-09-25 14:03:36 +0900

    Nav_bar생성

---
+++ src/components/Nav.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import "./Nav.css"
+
+export default function Nav() {
+
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+
+
+
+  }, []);
+
+
+  return (
+    <nav className={`nav ${show && "nav__black"}`}>
+      <img
+        alt="Netflix logo"
+        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        className='nav__logo'
+        onClick={() => window.location.reload()}
+      />
+      <img
+        alt="User logged"
+        src="https://occ-0-3996-1361.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABfNXUMVXGhnCZwPI1SghnGpmUgqS_J-owMff-jig42xPF7vozQS1ge5xTgPTzH7ttfNYQXnsYs4vrMBaadh4E6RTJMVepojWqOXx.png?r=1d4"
+        className='nav__avatar'
+      />
+    </nav>
+  )
+}
