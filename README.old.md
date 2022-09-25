cf7fd968c9e8d1e7f82246c22d96d689b5b1b15f
podoco <103816228+podoco@users.noreply.github.com>
2022-09-25 13:04:57 +0900

    Axios 요청

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# react-netflix-clone
+ 
---
+++ src/api/axios.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key: "6ce8fe6363dfcc789f95abfbd45c1990",
+    language: "ko-KR",
+  },
+})
