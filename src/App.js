2b8472dc99d1fa84fad5c5ccc448bc4391898715
podoco <103816228+podoco@users.noreply.github.com>
2022-09-29 13:17:13 +0900

    Row추가하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import requests from './api/requests';
 import Banner from './components/Banner';
 import Nav from './components/Nav';
+import Row from './components/Row';
 function App() {
   return (
     <div className="App">
       <Nav />
       <Banner></Banner>
+      <Row
+        title="NETFLEX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
+
     </div >
   );
 }
