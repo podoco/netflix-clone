6be9bd80b8b1e498053adde7b9a928709a0b6386
podoco <103816228+podoco@users.noreply.github.com>
2022-10-06 20:45:23 +0900

    env설정

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "6ce8fe6363dfcc789f95abfbd45c1990",
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
     language: "ko-KR",
   },
 })
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import requests from "../api/requests";
 import "./Banner.css"
 import styled from "styled-components";
+
 export default function Banner() {
 
   const [movie, setMovie] = useState([]);
@@ -15,20 +16,18 @@ export default function Banner() {
 
   const fetchData = async () => {
     //현재 상영중인 영화 정보를 가져오기(여러 영화)
-    const request = await axios.get(requests.fetchNowPlaying);
+    const request = await axios.get(requests.fetchNowPlaying);   //movie/nowplaying
 
     //여려 영화 중 영화 하나의 id를 가져오기
-    const movieId = request.data.results[
-      Math.floor(Math.random() * request.data.results.length)
-    ].id
+    const movieId = request.data.results[Math.floor(Math.random() * request.data.results.length)].id
 
     //  특정 영화의 더 상세한 정보를 가져오기 (비디오 정보도 포함)
     const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
       params: { append_to_response: "videos" },
     });
-
     setMovie(movieDetail);
   }
+
 
   const truncate = (str, n) => {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str
@@ -51,9 +50,10 @@ export default function Banner() {
             <button className='banner__button info'>More Information</button>
           </div>
 
-          <h1 className='banner__description'>{truncate(movie.overview, 100)}</h1>
+          <h1 className='banner__description'>{truncate(movie.overview, 250)}</h1>
 
         </div>
+
         <div className='banner--fadeBottom' />
       </header>
     );
@@ -93,7 +93,6 @@ const Iframe = styled.iframe`
     height: 100%;
   }
 `
-
 
 const Container = styled.div`
   display: flex;
